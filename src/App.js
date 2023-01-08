abb340c9fdea0bbc16fe97b4458bf5fc6f722b6c
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-01-08 19:40:57 +0530

    Initialize project by adding Webpack & Babel from scratch

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css'
+
+const App = () => (
+  <div className='app'>
+    <h1>hello</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
