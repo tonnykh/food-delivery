0bf74b96fe9edc0b163146b4e340f173baa88143
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-01-09 11:31:13 +0530

    Modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/App.js",
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
@@ -33,4 +33,5 @@ module.exports = {
       },
     ],
   },
+ 
 };
