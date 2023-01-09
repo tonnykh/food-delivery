79f468cb47966bd6f424bef007e3030127b4579c
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-01-09 11:14:06 +0530

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import "./style.css";
+
+/**
+ *
+ * Header
+ *   - Logo
+ *   - Nav Items(right side)
+ *   - Cart
+ * Body
+ *   - Search bar
+ *   - RestaurantList
+ *      - Image
+ *      - Name
+ *      - Rating
+ *      - Cuisines
+ * Footer
+ *   - links
+ *   - Copyright
+ *
+ */
 
 const App = () => (
-  <div className='app'>
-    <h1>hello</h1>
-  </div>
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
 );
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
