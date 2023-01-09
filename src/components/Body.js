5f1c2f6b27a97c2ed4f5eb30628fa8147d5eaaa6
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-01-09 16:56:15 +0530

    Add search box with functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,50 @@
 import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../config";
+import { useState } from "react";
+
+function filterData(searchTxt, restaurantList) {
+  const filterData = restaurantList.filter((restaurant) =>
+    restaurant.data.name.toLowerCase().includes(searchTxt.toLowerCase())
+  );
+
+  return filterData;
+}
 
 const Body = () => {
+  const [searchTxt, setSearchTxt] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
   return (
-    <main className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
-      })}
+    <main>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchTxt}
+          onChange={(e) => {
+            setSearchTxt(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchTxt, restaurantList);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
     </main>
   );
 };
