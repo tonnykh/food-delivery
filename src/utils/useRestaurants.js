a0c084a120b0f19e5d36c714244aec8c11aec9f3
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-03-06 16:21:29 +0530

    Add netlifi config

--- src/utils/useRestaurants.js
+++ src/utils/useRestaurants.js
@@ -15,7 +15,7 @@ const useRestaurants = () => {
        '/api//dapi/restaurants/list/v5?page_type=DESKTOP_WEB_LISTING&lat=28.5619207&lng=77.2112073'
      );
     const json = await response.json();
-    console.log(json.data, 'DATA CALL 1st');
+    console.log(json.data.cards, '-----DATA CALL 1st');
     // const restaurantData = data?.cards[2]?.data?.data?.cards;
     // const carouselsData = data?.cards[0]?.data?.data?.cards;
     // const mainCategories = data?.sorts;
